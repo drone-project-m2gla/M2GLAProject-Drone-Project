2ba69f05c0270da2594a497520979c4366f700ca
Fernando DIAZ <ferxdiaz@bitbucket.com>
2015-04-08 16:17:04 +0200

    test android svg

---
+++ DroneApplication/app/src/main/java/fr/m2gla/istic/projet/activity/SVGActivity.java
@@ -0,0 +1,35 @@
+package fr.m2gla.istic.projet.activity;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.PictureDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.caverock.androidsvg.SVG;
+import com.caverock.androidsvg.SVGParseException;
+
+public class SVGActivity extends Activity {
+    private ImageView imageView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.svg_layout);
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+
+        imageView1.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        try
+        {
+            SVG svg = SVG.getFromResource(this, R.raw.point_revitaillement);
+            Drawable drawable = new PictureDrawable(svg.renderToPicture());
+            imageView1.setImageDrawable(drawable);
+        }
+        catch(SVGParseException e)
+        {}
+    }
+}
