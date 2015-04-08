7d1e9082bcf07db8d1637a77ed0fca1b10e1a657
jerem <x>
2015-04-08 10:54:52 +0200

    Implementation Icons

--- server-rest/src/main/java/dao/IconDAO.java
+++ server-rest/src/main/java/dao/IconDAO.java
@@ -18,7 +18,7 @@ public class IconDAO extends AbstractDAO<Icon> {
             if (Constant.DATATYPE_ICON.equals(content.getString("datatype"))) {
                 u.setId(Long.parseLong(jsonDocument.id()));
                 u.setAddress_file(content.getString("MON ADRESSE !"));
-                u.setId(7878);
+                u.setId(1234);
             } else {
                 throw new IllegalArgumentException();
             }
@@ -31,10 +31,14 @@ public class IconDAO extends AbstractDAO<Icon> {
     @Override
     protected JsonDocument entityToJsonDocument(Icon u) {
 
+        JsonObject properties = JsonObject.create();
+        properties.put("datatype", u.getDataType());
+
         JsonObject jsonUser = JsonObject.empty()
-                .put("datatype", u.getDataType())
                 .put("adress", u.getAddress_file())
+                .put("properties", properties)
                 .put("id", u.getId());
+
         JsonDocument doc = JsonDocument.create("" + u.getId(), jsonUser);
         System.out.println(jsonUser);
         return doc;
--- server-rest/src/main/java/rest/Topographie.java
+++ server-rest/src/main/java/rest/Topographie.java
@@ -1,7 +1,46 @@
 package rest;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import dao.CoordinatedIconDAO;
+import entity.CoordinatedIcon;
+import entity.Position;
+
 /**
  * Created by jerem on 08/04/15.
+ *
+ * Envoie  l'ensemble des icons
  */
+@Path("/topographie")
 public class Topographie {
+
+
+    /**
+     * Retourner une liste json de points statiques sur la carte autour de la GeoPosition précisée en paramètre.
+     * Par défaut, on peut prendre une valeur de 50km autour.
+     * @param aGeoPosition
+     * @return
+     */
+        @GET
+        @Path("{GeoPosition}")
+        @Produces(MediaType.APPLICATION_JSON)
+        public List<CoordinatedIcon> getCoordinatedIcons(@PathParam("GeoPosition") Position aGeoPosition) {
+
+            CoordinatedIconDAO gID = new CoordinatedIconDAO();
+            gID.connect();
+            List<CoordinatedIcon> res = gID.getAll();
+            gID.disconnect();
+            return res;
+
+          //  return Response.status(200).entity("Le drone id est : " + aGeoPosition).build();
+
+        }
+
+
 }
