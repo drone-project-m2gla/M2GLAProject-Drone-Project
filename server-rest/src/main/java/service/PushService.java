4cdc4e9b7542e0902dbd265082edbc046359127c
baptiste_martin <martin_baptiste@live.fr>
2015-04-09 13:42:59 +0200

    add logger

--- server-rest/src/main/java/service/PushService.java
+++ server-rest/src/main/java/service/PushService.java
@@ -2,8 +2,10 @@ package service;
 
 import java.io.IOException;
 
+import com.google.android.gcm.server.MulticastResult;
+
 public interface PushService {
 	public void registerClient(String idClient);
 	public void unregisterClient(String idClient);
-	public void sendMessage(String scope, String message) throws IOException;
+	public MulticastResult sendMessage(String scope, String message) throws IOException;
 }
