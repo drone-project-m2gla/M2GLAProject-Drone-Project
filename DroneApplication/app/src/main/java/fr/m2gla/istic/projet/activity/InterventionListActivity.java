8ad06060813dfd912f8733413cce457ad3028498
david loubaresse <david.loubaresse@etudiant.univ-rennes1.fr>
2015-04-09 17:52:10 +0200

    DL : Reprise après effacement par GIT !

--- DroneApplication/app/src/main/java/fr/m2gla/istic/projet/activity/InterventionListActivity.java
+++ DroneApplication/app/src/main/java/fr/m2gla/istic/projet/activity/InterventionListActivity.java
@@ -2,9 +2,11 @@
 package fr.m2gla.istic.projet.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import fr.m2gla.istic.projet.context.GeneralConstants;
+import fr.m2gla.istic.projet.context.UserQualification;
 
 /**
  * Created by david on 09/02/15.
@@ -20,14 +23,52 @@ import fr.m2gla.istic.projet.context.GeneralConstants;
 
 public class InterventionListActivity extends Activity {
 
-    private ListView idList;
-    private ArrayList<HashMap<String, String>> listItem;
-    private SimpleAdapter mSchedule;
+    private ListView                            idList;
+    private ArrayList<HashMap<String, String>>  listItem;
+    private SimpleAdapter                       mSchedule;
+    private UserQualification                   userQualification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intervention_list);
+
+        Intent              intent;
+        String              roleStr;
+        Button              addButton = (Button) findViewById(R.id.addInterButton);
+
+
+        intent = getIntent();
+
+        if (intent != null) {
+            roleStr = intent.getStringExtra(GeneralConstants.REF_ACT_ROLE);
+            Toast.makeText(getApplicationContext(), roleStr, Toast.LENGTH_SHORT).show();
+
+            if (roleStr.compareTo(UserQualification.CODIS.toString()) == 0) {
+                Toast.makeText(getApplicationContext(), " - CODIS - ", Toast.LENGTH_SHORT).show();
+                this.userQualification = UserQualification.CODIS;
+            }
+            else if (roleStr.compareTo(UserQualification.SIMPLEUSER.toString()) == 0) {
+                Toast.makeText(getApplicationContext(), " - Sapeur - ", Toast.LENGTH_SHORT).show();
+                this.userQualification = UserQualification.SIMPLEUSER;
+                addButton.setEnabled(false);
+                addButton.setVisibility(View.INVISIBLE);
+            }
+            else {
+                Toast.makeText(getApplicationContext(), " - Aucun - ", Toast.LENGTH_SHORT).show();
+                this.userQualification = UserQualification.SIMPLEUSER;
+            }
+
+        } else {
+            Toast.makeText(getApplicationContext(), "PAS D'INTENT !", Toast.LENGTH_SHORT).show();
+        }
+
+        refreshList();
+
+    }
+
+
+    private void refreshList() {
 
         //Récupération de la listview créée dans le fichier clients.xml
         this.idList = (ListView) findViewById(R.id.idListView);
@@ -72,6 +113,12 @@ public class InterventionListActivity extends Activity {
     }
 
 
+    public void interventionRefresh(View view) {
+        // Demander le rafraichissement de la liste
+        refreshList();
+    }
+
+
     private void addInterventionInList(String code, String data) {
         // Verifier si une insertion est a faire
         if ((code == null) || (code.length() <= 0)) {
@@ -103,11 +150,11 @@ public class InterventionListActivity extends Activity {
 
 
     public void interventionSelection(View view) {
-        // Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        Intent intent = new Intent(getApplicationContext(), CodisActivity.class);
 
-        //lancement de la seconde activité, en demandant un code retour
+        // lancement de la seconde activité, en demandant un code retour
         // startActivityForResult(intent, 0);
-
+        startActivity(intent);
     }
 
 
