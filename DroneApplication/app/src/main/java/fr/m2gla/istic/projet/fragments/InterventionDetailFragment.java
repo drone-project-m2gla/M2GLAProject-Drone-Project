270f3dbc4dd0bc06e9943ca8815522aa61663ce0
MrChaabi <chaabi@CHAABI-Master2GL>
2015-04-14 18:20:41 +0200

    add: logs for tests

--- DroneApplication/app/src/main/java/fr/m2gla/istic/projet/fragments/InterventionDetailFragment.java
+++ DroneApplication/app/src/main/java/fr/m2gla/istic/projet/fragments/InterventionDetailFragment.java
@@ -15,6 +15,7 @@ import fr.m2gla.istic.projet.command.Command;
 import fr.m2gla.istic.projet.constantes.Constant;
 import fr.m2gla.istic.projet.context.RestAPI;
 import fr.m2gla.istic.projet.model.Intervention;
+import fr.m2gla.istic.projet.model.Mean;
 import fr.m2gla.istic.projet.service.impl.RestServiceImpl;
 
 import java.util.ArrayList;
@@ -35,7 +36,6 @@ public class InterventionDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_intervention_detail, container, false);
-
 
         titles = new String[]{Constant.VALUE_VEHICULE_EPA, Constant.VALUE_VEHICULE_FPT,
                 Constant.VALUE_VEHICULE_VSR, Constant.VALUE_VEHICULE_VLCG, Constant.VALUE_VEHICULE_VSAV};
@@ -60,17 +60,42 @@ public class InterventionDetailFragment extends Fragment {
 
         listMoyen.setAdapter(new ItemsAdapter(getActivity(), R.layout.custom_detail_moyen, titles,  imagesArray));
 
-
 //        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,, listMoyen);
 //        setListAdapter(adapter);
 //
-
-
         return view;
 
     }
 
 
+/*    public List<Mean> afficherListOfMoyens(String idIntervention){
+
+        titles = new String[]{Constant.VALUE_VEHICULE_EPA, Constant.VALUE_VEHICULE_FPT,
+                Constant.VALUE_VEHICULE_VSR, Constant.VALUE_VEHICULE_VLCG, Constant.VALUE_VEHICULE_VSAV};
+
+        images = new int[]{Constant.DRAWABLE_IMG_VEHICULE_EPA, Constant.DRAWABLE_IMG_VEHICULE_FPT,
+                Constant.DRAWABLE_IMG_VEHICULE_VSR, Constant.DRAWABLE_IMG_VEHICULE_VLCG, Constant.DRAWABLE_IMG_VEHICULE_VSAV};
+
+        List<Drawable> drawables = new ArrayList<Drawable>();
+        for (int imageId : images) {
+            if (imageId!=0) {
+                drawables.add(getResources().getDrawable(imageId));
+            } else {
+                drawables.add(getResources().getDrawable(R.drawable.bubble_shadow));
+            }
+        }
+
+
+        ListView listMoyen = (ListView) view.findViewById(R.id.intervention_detail_list);
+        Drawable[] imagesArray = drawables.toArray(new Drawable[drawables.size()]);
+
+        Log.i(TAG, TAG+"\nTaille   "+imagesArray.length);
+
+        listMoyen.setAdapter(new ItemsAdapter(getActivity(), R.layout.custom_detail_moyen, titles,  imagesArray));
+
+    }
+
+  */
 
     public void setIdIntervention(String idIntervention) {
         this.idIntervention = idIntervention;
@@ -81,13 +106,17 @@ public class InterventionDetailFragment extends Fragment {
             Map<String,String> map = new HashMap<>();
             map.put("id",idIntervention);
             RestServiceImpl.getInstance()
-                    .get(RestAPI.GET_INTERVENTION,map, Intervention.class,new Command() {
+                    .get(RestAPI.GET_INTERVENTION, map, Intervention.class, new Command() {
                         @Override
                         public void execute(Object response) {
-                           Intervention intervention = (Intervention)response;
-                            Toast.makeText(getActivity(),"  test intervetion return " + intervention.getId(),Toast.LENGTH_LONG).show();
+                            Intervention intervention = (Intervention) response;
+                            Toast.makeText(getActivity(), "  test intervetion return " + intervention.getId(), Toast.LENGTH_LONG).show();
+                            int i = 0;
+                            for (Mean m :intervention.getMeansXtra()){
+                            Log.i(TAG, TAG+"\nListe of moyen " + m.getVehicle().toString() + i++);}
+
                         }
-                    },new Command() {
+                    }, new Command() {
                         @Override
                         public void execute(Object response) {
 
