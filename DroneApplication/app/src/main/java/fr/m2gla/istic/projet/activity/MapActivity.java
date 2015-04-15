685a0813b6bb4942ace82aa5b9fe2914c63cfd5c
Fernando DIAZ <ferxdiaz@bitbucket.com>
2015-04-15 17:47:47 +0200

    MapActivity cleanup

--- DroneApplication/app/src/main/java/fr/m2gla/istic/projet/activity/MapActivity.java
+++ DroneApplication/app/src/main/java/fr/m2gla/istic/projet/activity/MapActivity.java
@@ -42,9 +42,14 @@ import fr.m2gla.istic.projet.model.Topographie;
 import fr.m2gla.istic.projet.service.impl.RestServiceImpl;
 
 public class MapActivity extends Activity implements
-        ClusterManager.OnClusterItemInfoWindowClickListener<SymbolMarkerClusterItem>{
+        ClusterManager.OnClusterItemInfoWindowClickListener<SymbolMarkerClusterItem>,
+        AdapterView.OnDragListener,
+        GoogleMap.OnMarkerDragListener,
+        GoogleMap.OnMapLongClickListener
+
+{
     private static final String TAG = "MapActivity";
-    // offset to place the icon when dropped
+    // offsets used to place the icon when it is dropped
     private static final int OFFSET_X = -100;
     private static final int OFFSET_Y = 30;
 
@@ -53,12 +58,13 @@ public class MapActivity extends Activity implements
     private GoogleMap map;
     private ClusterManager<SymbolMarkerClusterItem> mClusterManager;
     // Keep track of all the symbols by marker id
-    private Map<String, SymbolMarkerClusterItem> markerSymbolLink = new HashMap<String, SymbolMarkerClusterItem>();
+    private Map<String, SymbolMarkerClusterItem> markerSymbolLink;
 
     // default latitude and longitude to center map if error
     private double latitude = 48.1119800 ;
     private double longitude = -1.6742900;
 
+    // Shift used by raise on drag for map markers
     private static final int SHIFT_RAISE_ON_DRAG = 90;
 
     private Map<String, String> param;
@@ -71,122 +77,27 @@ public class MapActivity extends Activity implements
         setContentView(R.layout.activity_map);
 
         param = new HashMap<String, String>();
-        Intent intent = getIntent();
-
-        if (intent != null) {
-            String idIntervention = intent.getStringExtra(GeneralConstants.ID_INTERVENTION);
-            Toast.makeText(getApplication(), "Bonjour\nID intervention " + idIntervention, Toast.LENGTH_LONG);
-            MoyensSuppFragment mSuppFragment = (MoyensSuppFragment) getFragmentManager().findFragmentById(R.id.fragment_moyens_supp);
-
-            if (mSuppFragment != null){
-                mSuppFragment.setInterventionID(idIntervention);
-            }
-
-            param.put("id", idIntervention);
-
-            RestServiceImpl.getInstance()
-                    .get(RestAPI.GET_INTERVENTION, param, Intervention.class,
-                        new Command() {
-                            @Override
-                            public void execute(Object response) {
-                                Intervention intervention = (Intervention)response;
-                                Position pos = intervention.getCoordinates();
-                                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(pos.getLatitude(), pos.getLongitude()), 15));
-                                loadSymbols();
-                            }
-                        },
-                        new Command() {
-                            @Override
-                            public void execute(Object response) {
-                                Log.e(TAG, "Error get intervention");
-                            }
-                        });
-        }
+        markerSymbolLink = new HashMap<String, SymbolMarkerClusterItem>();
 
         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-
         map = mapFragment.getMap();
-        addDroneListener();
+
         map.getUiSettings().setCompassEnabled(true);
 
-        map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
-            @Override
-            public void onMarkerDragStart(Marker marker) {
-                // TODO: mettre en pointillés les icones
-                LatLng latLng = marker.getPosition();
-                Point point = map.getProjection().toScreenLocation(latLng);
-                point.set(point.x, point.y + SHIFT_RAISE_ON_DRAG);
-                LatLng latLng2 = map.getProjection().fromScreenLocation(point);
-                marker.setPosition(latLng2);
-            }
+        loadIntervention();
+        // Set map's long click listener to control drone
+        map.setOnMapLongClickListener(this);
 
-            @Override
-            public void onMarkerDrag(Marker marker) {
-                disableRaiseOnDrag(marker);
-            }
-
-            @Override
-            public void onMarkerDragEnd(Marker marker) {
-                if (markerSymbolLink.containsKey(marker.getId())){
-                    Log.d(TAG, "onMarkerDragStart found " + marker.getId());
-                    Symbol symbol =  markerSymbolLink.get(marker.getId()).getSymbol();
-                    symbol.setValidated(false);
-
-                    marker.setIcon(SVGAdapter.convertSymbolToIcon(getApplicationContext(), symbol));
-                }
-                disableRaiseOnDrag(marker);
-                Position position = new Position();
-                position.setLatitude(marker.getPosition().latitude);
-                position.setLongitude(marker.getPosition().longitude);
-                Mean mean = new Mean();
-                mean.setId("");
-                mean.setCoordinates(position);
-
-                RestServiceImpl.getInstance()
-                    .post(RestAPI.POST_POSITION_MOVE, param, mean, Mean.class,
-                            new Command() {
-                                @Override
-                                public void execute(Object response) {
-                                    Log.e(TAG, "Post new position success");
-                                }
-                            },
-                            new Command() {
-                                @Override
-                                public void execute(Object response) {
-                                    Log.e(TAG, "Post new position error");
-                                }
-                            });
-            }
-        });
+        // Set marker's drag listener to control marker drag & drop behaviour
+        map.setOnMarkerDragListener(this);
 
         // Initialize the manager with the context and the map.
         // (Activity extends context, so we can pass 'this' in the constructor.)
         mClusterManager = new ClusterManager<>(this, map);
         mClusterManager.setRenderer(new SymbolRendered(this, map, mClusterManager));
 
-        mapFragment.getView().setOnDragListener(new AdapterView.OnDragListener() {
-            @Override
-            public boolean onDrag(View v, DragEvent event) {
-            if (event.getAction() == DragEvent.ACTION_DROP) {
-                Log.i(TAG, event.toString());
-
-                ClipData clipData = event.getClipData();
-                try {
-                    //Get symbol name from ClipData saved onDrag
-                    Symbol.SymbolType symbolType = Symbol.SymbolType.valueOf((String)clipData.getItemAt(0).getText());
-                    Log.d(TAG, clipData.getItemAt(0).toString());
-                    LatLng latlng = map.getProjection().fromScreenLocation(new Point((int) event.getX() + OFFSET_X, (int) event.getY() + OFFSET_Y));
-                    Symbol symbol = new Symbol(symbolType, "AAA", "BBB", "ff0000", "Description");
-                    symbol.setValidated(true);
-                    SymbolMarkerClusterItem markerItem = new SymbolMarkerClusterItem(latlng.latitude, latlng.longitude, symbol);
-                    mClusterManager.addItem(markerItem);
-                    mClusterManager.cluster();
-
-                } catch (IllegalArgumentException e){}
-            }
-            return true;
-            }
-        });
+        // Set map fragment drag listener to recover dropped symbols
+        mapFragment.getView().setOnDragListener(this);
 
         mClusterManager.setOnClusterItemInfoWindowClickListener(this);
 
@@ -194,8 +105,9 @@ public class MapActivity extends Activity implements
         // manager.
         map.setOnCameraChangeListener(mClusterManager);
         map.setOnMarkerClickListener(mClusterManager);
+
+        // Enable info window click on each cluster element
         map.setOnInfoWindowClickListener(mClusterManager);
-        loadSymbols();
     }
 
     @Override
@@ -216,6 +128,9 @@ public class MapActivity extends Activity implements
         return true;
     }
 
+    /**
+     * Load symbols using topographic REST service
+     */
     public void loadSymbols() {
         RestServiceImpl.getInstance().get(RestAPI.GET_ALL_TOPOGRAPHIE, null, Topographie[].class,
         new Command() {
@@ -264,6 +179,11 @@ public class MapActivity extends Activity implements
 
     }
 
+    /**
+     * Item info window click listener
+     * Used to show a dialog on info window click to validate the new position
+     * @param symbolMarkerClusterItem
+     */
     @Override
     public void onClusterItemInfoWindowClick(SymbolMarkerClusterItem symbolMarkerClusterItem) {
         Log.d(TAG, "main onClusterItemInfoWindowClick");
@@ -302,34 +222,129 @@ public class MapActivity extends Activity implements
         }
     }
 
-    public void addDroneListener() {
-        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-            @Override
-            public void onMapLongClick(LatLng latLng) {
-                Position position = new Position();
-                position.setLatitude(latLng.latitude);
-                position.setLongitude(latLng.longitude);
+    /**
+     * Map fragment drag listener used to get the dropped symbol
+     * A new symbol is created and added to the map
+     * @param v current view
+     * @param event drag element
+     * @return
+     */
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        if (event.getAction() == DragEvent.ACTION_DROP) {
+            Log.i(TAG, event.toString());
 
-                RestServiceImpl.getInstance()
-                        .post(RestAPI.POST_POSITION_DRONE, null, position, Void.class,
-                                new Command() {
-                                    @Override
-                                    public void execute(Object response) {
-                                        Log.i(TAG, "Drone move");
-                                    }
-                                },
-                                new Command() {
-                                    @Override
-                                    public void execute(Object response) {
-                                        Log.e(TAG, "Push position error");
-                                    }
-                                });
-            }
-        });
+            ClipData clipData = event.getClipData();
+            try {
+                //Get symbol name from ClipData saved onDrag
+                Symbol.SymbolType symbolType = Symbol.SymbolType.valueOf((String)clipData.getItemAt(0).getText());
+                Log.d(TAG, clipData.getItemAt(0).toString());
+                LatLng latlng = map.getProjection().fromScreenLocation(new Point((int) event.getX() + OFFSET_X, (int) event.getY() + OFFSET_Y));
+                Symbol symbol = new Symbol(symbolType, "AAA", "BBB", "ff0000", "Description");
+                symbol.setValidated(true);
+                SymbolMarkerClusterItem markerItem = new SymbolMarkerClusterItem(latlng.latitude, latlng.longitude, symbol);
+                mClusterManager.addItem(markerItem);
+                mClusterManager.cluster();
+
+            } catch (IllegalArgumentException ignored){}
+        }
+        return true;
+    }
+
+    /**
+     * Marker drag listener method used when drag starts after a long click
+     * Used to disable raiseOnDrag
+     * @param marker dragged marker element
+     */
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        disableRaiseOnDrag(marker);
+    }
+
+    /**
+     * Marker drag listener method used when marker is dragged
+     * Used to disable raiseOnDrag
+     * @param marker dragged marker element
+     */
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        disableRaiseOnDrag(marker);
+    }
+
+    /**
+     * Marker drag listener method used when marker is dropped
+     * Changes symbol to use dashed lines whenever its position is changed
+     *
+     * @param marker dragged marker element
+     */
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        //Change symbol image to dashed one
+        if (markerSymbolLink.containsKey(marker.getId())){
+            Log.d(TAG, "onMarkerDragStart found " + marker.getId());
+            Symbol symbol =  markerSymbolLink.get(marker.getId()).getSymbol();
+            symbol.setValidated(false);
+            marker.setIcon(SVGAdapter.convertSymbolToIcon(getApplicationContext(), symbol));
+        }
+        //Disable raiseOnDrag
+        disableRaiseOnDrag(marker);
+
+        //Use REST to update position on confirmation
+        Position position = new Position();
+        position.setLatitude(marker.getPosition().latitude);
+        position.setLongitude(marker.getPosition().longitude);
+        Mean mean = new Mean();
+        mean.setId("");
+        mean.setCoordinates(position);
+
+        RestServiceImpl.getInstance()
+                .post(RestAPI.POST_POSITION_MOVE, param, mean, Mean.class,
+                        new Command() {
+                            @Override
+                            public void execute(Object response) {
+                                Log.e(TAG, "Post new position success");
+                            }
+                        },
+                        new Command() {
+                            @Override
+                            public void execute(Object response) {
+                                Log.e(TAG, "Post new position error");
+                            }
+                        });
+    }
+
+    /**
+     * Map long click listener
+     * Allows to control drone
+     * @param latLng current long click position
+     */
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        Position position = new Position();
+        position.setLatitude(latLng.latitude);
+        position.setLongitude(latLng.longitude);
+
+        RestServiceImpl.getInstance()
+           .post(RestAPI.POST_POSITION_DRONE, null, position, Void.class,
+                new Command() {
+                    @Override
+                    public void execute(Object response) {
+                        Log.i(TAG, "Drone move");
+                    }
+                },
+                new Command() {
+                    @Override
+                    public void execute(Object response) {
+                        Log.e(TAG, "Push position error");
+                    }
+                });
     }
 
     /**
      * Cluster rendered used to draw firemen symbols
+     * All the symbols are integrated into the map that links markers and symbols
+     * Non-topographic symbols are draggable
+     * Description is shown in marker's info window
      */
     public class SymbolRendered extends DefaultClusterRenderer<SymbolMarkerClusterItem> {
         public SymbolRendered(Context context, GoogleMap map, ClusterManager<SymbolMarkerClusterItem> clusterManager) {
@@ -354,6 +369,11 @@ public class MapActivity extends Activity implements
         }
     }
 
+    /**
+     * Whenever a marker starts to be dragged it is raised,
+     * this method allows to undo this shift to simulate raiseOnDrag = false behaviour
+     * @param marker dragged marker
+     */
     private void disableRaiseOnDrag(Marker marker){
         LatLng latLng = marker.getPosition();
         Point point = map.getProjection().toScreenLocation(latLng);
@@ -361,4 +381,42 @@ public class MapActivity extends Activity implements
         LatLng latLng2 = map.getProjection().fromScreenLocation(point);
         marker.setPosition(latLng2);
     }
+
+    /**
+     * Loads current intervention symbols
+     */
+    private void loadIntervention(){
+        Intent intent = getIntent();
+
+        if (intent != null) {
+            String idIntervention = intent.getStringExtra(GeneralConstants.ID_INTERVENTION);
+            Toast.makeText(getApplication(), "Bonjour\nID intervention " + idIntervention, Toast.LENGTH_LONG).show();
+            MoyensSuppFragment mSuppFragment = (MoyensSuppFragment) getFragmentManager().findFragmentById(R.id.fragment_moyens_supp);
+
+            if (mSuppFragment != null){
+                mSuppFragment.setInterventionID(idIntervention);
+            }
+
+            param.put("id", idIntervention);
+
+            RestServiceImpl.getInstance()
+            .get(RestAPI.GET_INTERVENTION, param, Intervention.class,
+                new Command() {
+                    @Override
+                    public void execute(Object response) {
+                        Intervention intervention = (Intervention)response;
+                        Position pos = intervention.getCoordinates();
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(pos.getLatitude(), pos.getLongitude()), 15));
+                        loadSymbols();
+                    }
+                },
+                new Command() {
+                    @Override
+                    public void execute(Object response) {
+                        Log.e(TAG, "Error get intervention");
+                    }
+                });
+        }
+    }
+
 }
