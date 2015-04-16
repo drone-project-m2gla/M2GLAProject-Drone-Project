9f7f73a6a42bb7d8779ad3448bdf17e2354ba43d
mdiansow <mdiansow@yahoo.fr>
2015-04-16 09:45:48 +0200

    ItemsAdapter --> Utilisation de ViewHolder

--- DroneApplication/app/src/main/java/fr/m2gla/istic/projet/fragments/ItemsAdapter.java
+++ DroneApplication/app/src/main/java/fr/m2gla/istic/projet/fragments/ItemsAdapter.java
@@ -38,49 +38,152 @@ public class ItemsAdapter extends ArrayAdapter {
         customLayout = textViewResourceId;
     }
 
+    // It gets a View that displays the data at the specified position
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    //    // declare it final so that it could be accessed from the inner class
+//    final ViewHolder holder;
+//
+//    if (convertView == null)
+//    {
+//        LayoutInflater mInflater = (LayoutInflater) SimpleAddressBookAdapter.context
+//                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//
+//        convertView = mInflater.inflate(SimpleAddressBookAdapter.resource, parent, false);
+//
+//        holder = new ViewHolder();
+//        holder.textView = new TextView[fromList.length];
+//
+//        // get the textview's from the convertView
+//        for (int i = 0; i < fromList.length; i++) {
+//            holder.textView[i] = (TextView) convertView.findViewById(resourceList[i]);
+//        }
+//
+//        // get the phoneIcon and emailIcon as well from convertView
+//        holder.phoneIcon = (ImageView) convertView.findViewById(R.id.address_book_item_call);
+//        holder.emailIcon = (ImageView) convertView.findViewById(R.id.address_book_item_email);
+//
+//        // add a listener for phone call
+//        holder.phoneIcon.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//                String phone = SimpleAddressBookAdapter.listMap.get(holder.position).get("phone");
+//                ActivityHelper.startActivity(ActivityHelper.PHONE_CALL, phone);
+//            }
+//
+//        });
+//
+//        // add listener for email
+//        holder.emailIcon.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//                String email = SimpleAddressBookAdapter.listMap.get(holder.position).get("email");
+//                ActivityHelper.startActivity(ActivityHelper.EMAIL, email);
+//            }
+//
+//        });
+//
+//        // store it in a Tag as its the first time this view is generated
+//        convertView.setTag(holder);
+//
+//    }
+//    else {
+//			/* get the View from the existing Tag */
+//        holder = (ViewHolder) convertView.getTag();
+//    }
+//
+//		/* update the textView's text for this list view item/element */
+//    for (int i = 0; i < fromList.length; i++) {
+//        holder.textView[i].setText(listMap.get(position).get(fromList[i]));
+//    }
+//
+//    // store the position/index for this list view element/item
+//    holder.position = position;
     public View getCustomView(int position, View convertView,
                               ViewGroup parent) {
 
-        // Inflating the layout for the custom Spinner
-        LayoutInflater inflater = activity.getLayoutInflater();
-        View layout = inflater.inflate(this.customLayout, parent, false);
+        final ViewHolder holder;
 
-        // Declaring and Typecasting the textview in the inflated layout
-        TextView itemLabelTxtView = (TextView) layout
-                .findViewById(R.id.tvLanguage);
+        if (convertView == null) {
+            LayoutInflater mInflater = activity.getLayoutInflater();
 
-        // Setting the text using the array
-        itemLabelTxtView.setText(titles[position]);
+            convertView = mInflater.inflate(this.customLayout, parent, false);
 
-        // Setting the color of the text
-        itemLabelTxtView.setTextColor(Color.rgb(75, 180, 225));
+            holder = new ViewHolder();
+            holder.itemLabelTxtView = (TextView) convertView
+                    .findViewById(R.id.tvLanguage);
+            // get the phoneIcon and emailIcon as well from convertView
+            holder.imgImageView = (ImageView) convertView.findViewById(R.id.imgLanguage);
 
-        // Declaring and Typecasting the imageView in the inflated layout
-        ImageView imgImageView = (ImageView) layout.findViewById(R.id.imgLanguage);
+            // Setting Special attributes for 1st element
+            if (position == 0 && titles[position].equals("")) {
+                // Removing the image view
+                holder.imgImageView.setVisibility(View.GONE);
+                // Setting the size of the text
+                holder.itemLabelTxtView.setTextSize(20f);
+                // Setting the text Color
+                holder.itemLabelTxtView.setTextColor(Color.WHITE);
+                // Setting the value
+                holder.itemLabelTxtView.setText("Sélectionner un moyen supp.");
 
-//        Log.i(TAG, "image OUT " + position + "   is   " + (images[position] == null));
-
-        // Setting Special attributes for 1st element
-        if (position == 0 && titles[position].equals("")) {
-            // Removing the image view
-            imgImageView.setVisibility(View.GONE);
-            // Setting the size of the text
-            itemLabelTxtView.setTextSize(20f);
-            // Setting the text Color
-            itemLabelTxtView.setTextColor(Color.WHITE);
-            // Setting the value
-            itemLabelTxtView.setText("Sélectionner un moyen supp.");
-
-        } else {
-            Drawable drawable = images[position];
-            Log.i(TAG, "drawable  " + position + "   is   " + (drawable == null));
-            Log.i(TAG, "image  " + position + "   is   " + (images[position] == null));
-            Bitmap src = SVGAdapter.convertDrawableToBitmap(drawable, 64, 64);
-            Log.i(TAG, "src  " + position + "   is   " + (src == null));
-            Bitmap image = Bitmap.createScaledBitmap(src, 50, 50, true);
-            imgImageView.setImageBitmap(image);
+            } else {
+                Drawable drawable = images[position];
+                Log.i(TAG, "drawable  " + position + "   is   " + (drawable == null));
+                Log.i(TAG, "image  " + position + "   is   " + (images[position] == null));
+                Bitmap src = SVGAdapter.convertDrawableToBitmap(drawable, 64, 64);
+                Log.i(TAG, "src  " + position + "   is   " + (src == null));
+                Bitmap image = Bitmap.createScaledBitmap(src, 50, 50, true);
+                holder.imgImageView.setImageBitmap(image);
+            }
         }
-        return layout;
+
+//        // Inflating the layout for the custom Spinner
+//        LayoutInflater inflater = activity.getLayoutInflater();
+//        View layout = inflater.inflate(this.customLayout, parent, false);
+//
+//        // Declaring and Typecasting the textview in the inflated layout
+//        TextView itemLabelTxtView = (TextView) layout
+//                .findViewById(R.id.tvLanguage);
+//
+//        // Setting the text using the array
+//        itemLabelTxtView.setText(titles[position]);
+//
+//        // Setting the color of the text
+//        itemLabelTxtView.setTextColor(Color.rgb(75, 180, 225));
+//
+//        // Declaring and Typecasting the imageView in the inflated layout
+//        ImageView imgImageView = (ImageView) layout.findViewById(R.id.imgLanguage);
+//
+//
+//        // Setting Special attributes for 1st element
+//        if (position == 0 && titles[position].equals("")) {
+//            // Removing the image view
+//            imgImageView.setVisibility(View.GONE);
+//            // Setting the size of the text
+//            itemLabelTxtView.setTextSize(20f);
+//            // Setting the text Color
+//            itemLabelTxtView.setTextColor(Color.WHITE);
+//            // Setting the value
+//            itemLabelTxtView.setText("Sélectionner un moyen supp.");
+//
+//        } else {
+//            Drawable drawable = images[position];
+//            Log.i(TAG, "drawable  " + position + "   is   " + (drawable == null));
+//            Log.i(TAG, "image  " + position + "   is   " + (images[position] == null));
+//            Bitmap src = SVGAdapter.convertDrawableToBitmap(drawable, 64, 64);
+//            Log.i(TAG, "src  " + position + "   is   " + (src == null));
+//            Bitmap image = Bitmap.createScaledBitmap(src, 50, 50, true);
+//            imgImageView.setImageBitmap(image);
+//        }
+//        return layout;
+        return convertView;
     }
 
     // It gets a View that displays in the drop down popup the data at the specified position
@@ -90,9 +193,12 @@ public class ItemsAdapter extends ArrayAdapter {
         return getCustomView(position, convertView, parent);
     }
 
-    // It gets a View that displays the data at the specified position
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return getCustomView(position, convertView, parent);
+    /* A Static class for holding the elements of each List View Item
+     * This is created as per Google UI Guideline for faster performance */
+    private static class ViewHolder {
+        TextView itemLabelTxtView;
+        ImageView imgImageView;
+
+        int position;
     }
 }
