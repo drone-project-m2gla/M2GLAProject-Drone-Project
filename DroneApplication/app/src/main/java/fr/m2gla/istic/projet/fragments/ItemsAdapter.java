d12515773fdd1114c5237ff5bcdf7c3b44922df9
MrChaabi <chaabi@CHAABI-Master2GL>
2015-04-16 13:31:13 +0200

    add : ajout des listner dans les button valider et annuller

--- DroneApplication/app/src/main/java/fr/m2gla/istic/projet/fragments/ItemsAdapter.java
+++ DroneApplication/app/src/main/java/fr/m2gla/istic/projet/fragments/ItemsAdapter.java
@@ -1,6 +1,7 @@
 package fr.m2gla.istic.projet.fragments;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
@@ -10,11 +11,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import fr.m2gla.istic.projet.activity.R;
+import fr.m2gla.istic.projet.command.Command;
+import fr.m2gla.istic.projet.context.RestAPI;
+import fr.m2gla.istic.projet.model.Intervention;
+import fr.m2gla.istic.projet.model.Mean;
 import fr.m2gla.istic.projet.model.SVGAdapter;
+import fr.m2gla.istic.projet.service.impl.RestServiceImpl;
 
 // Creating an Adapter Class
 public class ItemsAdapter extends ArrayAdapter {
@@ -26,6 +39,7 @@ public class ItemsAdapter extends ArrayAdapter {
 
     private String[] titles;
     private int customLayout;
+    Intervention intervention;
 
     public ItemsAdapter(Context context, int textViewResourceId,
                         String[] objects, Drawable[] images) {
@@ -36,6 +50,7 @@ public class ItemsAdapter extends ArrayAdapter {
         this.images = images;
         Log.i(TAG, "MeanXtra Image\t" + this.images.length);
         customLayout = textViewResourceId;
+
     }
 
     // It gets a View that displays the data at the specified position
@@ -44,9 +59,11 @@ public class ItemsAdapter extends ArrayAdapter {
         return getCustomView(position, convertView, parent);
     }
 
-    public View getCustomView(int position, View convertView, ViewGroup parent) {
+    public View getCustomView(final int position, View convertView, ViewGroup parent) {
 
         final ViewHolder holder;
+        Mean mean;
+        final Map<String, String> map = new HashMap<>();
 
         if (convertView == null) {
             LayoutInflater mInflater = activity.getLayoutInflater();
@@ -60,6 +77,61 @@ public class ItemsAdapter extends ArrayAdapter {
             // get the phoneIcon and emailIcon as well from convertView
             holder.imgImageView = (ImageView) convertView.findViewById(R.id.imgLanguage);
 
+            holder.annullerImageButton = (ImageButton) convertView.findViewById(R.id.annuler);
+            holder.validerImageButton = (ImageButton) convertView.findViewById(R.id.valid);
+
+
+            if (holder.annullerImageButton != null) {
+                InterventionDetailFragment interventionFragment = (InterventionDetailFragment) activity.getFragmentManager().findFragmentById(R.id.fragment_intervention_detail);
+
+                final String idIntervention = interventionFragment.getIdIntervention();
+                final Mean xtraMean = interventionFragment.getMeanXtra(position);
+
+                holder.annullerImageButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        map.put("idintervention", idIntervention);
+                        Toast.makeText(getContext(), "button annuller " + position, Toast.LENGTH_LONG).show();
+
+                        RestServiceImpl.getInstance()
+                                .post(RestAPI.POST_ANNULLER_MOYEN, map, xtraMean, Intervention.class,
+                                        new Command() {
+                                            @Override
+                                            public void execute(Object response) {
+                                                Toast.makeText(getContext(), "Moyen annuller\nID mean: " + xtraMean.getId(), Toast.LENGTH_LONG).show();
+                                            }
+                                        }, new Command() {
+                                            @Override
+                                            public void execute(Object response) {
+                                                Toast.makeText(getContext(), "Moyen pas été annuller\nID mean: " + xtraMean.getId(), Toast.LENGTH_LONG).show();
+                                            }
+                                        });
+
+                    }
+                });
+
+                holder.validerImageButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        map.put("idintervention", idIntervention);
+                        RestServiceImpl.getInstance()
+                                .post(RestAPI.POST_VALIDER_MOYEN, map, xtraMean, Intervention.class,
+                                        new Command() {
+                                            @Override
+                                            public void execute(Object response) {
+                                                Toast.makeText(getContext(), "Moyen validé\nID mean: " + xtraMean.getId(), Toast.LENGTH_LONG).show();
+                                            }
+                                        }, new Command() {
+                                            @Override
+                                            public void execute(Object response) {
+                                                Toast.makeText(getContext(), "Moyen n'a pas été validé\nID mean: " + xtraMean.getId(), Toast.LENGTH_LONG).show();
+                                            }
+                                        });
+                    }
+                });
+            }
             // Setting Special attributes for 1st element
             if (position == 0 && titles[position].equals("")) {
                 // Removing the image view
@@ -82,52 +154,12 @@ public class ItemsAdapter extends ArrayAdapter {
                 Bitmap image = Bitmap.createScaledBitmap(src, 50, 50, true);
                 holder.imgImageView.setImageBitmap(image);
             }
+            holder.position = position;
         } else {
             /* get the View from the existing Tag */
             holder = (ViewHolder) convertView.getTag();
         }
 
-        holder.position = position;
-
-//        // Inflating the layout for the custom Spinner
-//        LayoutInflater inflater = activity.getLayoutInflater();
-//        View layout = inflater.inflate(this.customLayout, parent, false);
-//
-//        // Declaring and Typecasting the textview in the inflated layout
-//        TextView itemLabelTxtView = (TextView) layout
-//                .findViewById(R.id.tvLanguage);
-//
-//        // Setting the text using the array
-//        itemLabelTxtView.setText(titles[position]);
-//
-//        // Setting the color of the text
-//        itemLabelTxtView.setTextColor(Color.rgb(75, 180, 225));
-//
-//        // Declaring and Typecasting the imageView in the inflated layout
-//        ImageView imgImageView = (ImageView) layout.findViewById(R.id.imgLanguage);
-//
-//
-//        // Setting Special attributes for 1st element
-//        if (position == 0 && titles[position].equals("")) {
-//            // Removing the image view
-//            imgImageView.setVisibility(View.GONE);
-//            // Setting the size of the text
-//            itemLabelTxtView.setTextSize(20f);
-//            // Setting the text Color
-//            itemLabelTxtView.setTextColor(Color.WHITE);
-//            // Setting the value
-//            itemLabelTxtView.setText("Sélectionner un moyen supp.");
-//
-//        } else {
-//            Drawable drawable = images[position];
-//            Log.i(TAG, "drawable  " + position + "   is   " + (drawable == null));
-//            Log.i(TAG, "image  " + position + "   is   " + (images[position] == null));
-//            Bitmap src = SVGAdapter.convertDrawableToBitmap(drawable, 64, 64);
-//            Log.i(TAG, "src  " + position + "   is   " + (src == null));
-//            Bitmap image = Bitmap.createScaledBitmap(src, 50, 50, true);
-//            imgImageView.setImageBitmap(image);
-//        }
-//        return layout;
         return convertView;
     }
 
@@ -144,6 +176,8 @@ public class ItemsAdapter extends ArrayAdapter {
     private static class ViewHolder {
         TextView itemLabelTxtView;
         ImageView imgImageView;
+        ImageButton validerImageButton;
+        ImageButton annullerImageButton;
 
         int position;
     }
