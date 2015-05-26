291fd77693aca84fb585f2bb43d55b1457d21a4e
jerem <x>
2015-05-26 09:52:21 +0200

    javadoc

--- server-rest/src/main/java/rest/PushRegister.java
+++ server-rest/src/main/java/rest/PushRegister.java
@@ -1,5 +1,9 @@
 package rest;
 
+import com.google.android.gcm.server.MulticastResult;
+
+import org.apache.log4j.Logger;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
@@ -10,15 +14,14 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.apache.log4j.Logger;
-
+import entity.PushEntity;
 import service.PushService.TypeClient;
 import service.impl.PushServiceImpl;
 
-import com.google.android.gcm.server.MulticastResult;
-
-import entity.PushEntity;
-
+/**
+ * @author baptiste on 08/04/15.
+ * @see PushRegister is service rest for push information in the Google Cloud Messaging
+ */
 @Path("/gcm")
 public class PushRegister {
 	private static final Logger LOGGER = Logger.getLogger(PushRegister.class);
@@ -30,14 +33,14 @@ public class PushRegister {
 		PushServiceImpl.getInstance().registerClient(entity.getTypeClient(), entity.getId());
 		LOGGER.info("User connect to push service " + entity.getId() + " " + entity.getTypeClient());
 	}
-	
+
 	@DELETE
 	@Path("unregister/{id}")
 	public void unregisterClient(@PathParam("id") String id) {
 		PushServiceImpl.getInstance().unregisterClient(id);
 		LOGGER.info("User disconect to push service " + id);
 	}
-	
+
 	@POST
 	@Path("register/add/{scope}")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -45,12 +48,12 @@ public class PushRegister {
 	public Response testPush(@PathParam("scope") String scope, Object object) {
 		//FIXME Pour les tests, a supprimer
 		Response response;
-		
-		try {			
+
+		try {
 			MulticastResult result = PushServiceImpl.getInstance().sendMessage(TypeClient.ALL, scope, object);
-			
+
 			if (result == null) throw new Exception("Not user");
-			
+
 			if (result.getSuccess() == 1) {
 				response =  Response.status(Status.CREATED).entity(result).build();
 			} else {
@@ -60,7 +63,7 @@ public class PushRegister {
 			LOGGER.error("Error push message", e);
 			response = Response.status(Status.INTERNAL_SERVER_ERROR).entity(e).build();
 		}
-		
+
 		return response;
 	}
 }
