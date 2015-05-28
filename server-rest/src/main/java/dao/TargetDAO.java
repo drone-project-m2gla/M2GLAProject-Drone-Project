56095e2299b049674ae81698731e38bde4cf4948
baptiste_martin <martin_baptiste@live.fr>
2015-05-28 10:17:57 +0200

    add class forget

---
+++ server-rest/src/main/java/dao/TargetDAO.java
@@ -0,0 +1,48 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
+import util.Constant;
+import util.Tools;
+import entity.Target;
+
+public class TargetDAO extends AbstractDAO<Target> {
+	/**
+	 * Contructor TargetDAO
+	 */
+	public TargetDAO() {
+		this.datatype = Constant.DATATYPE_GEOIMAGE;
+	}
+
+	@Override
+	protected Target documentToEntity(Document document) {
+		if (document == null) {
+			return null;
+		}
+		
+		Target target = new Target();
+		target.setId(document.getLong("_id"));
+		target.setInterventionId(document.getInteger("interventionId"));
+		target.setClose(document.getBoolean("isClose"));
+		target.setPositions(Tools.documentListToPositionList(((List) document.get("positions"))));
+		return target;
+	}
+
+	@Override
+	protected Document entityToDocument(Target entity) {
+		if (entity == null) {
+			return null;
+		}
+		
+		Document document = new Document();
+		document.put("_id", entity.getId());
+		document.put("interventionId", entity.getInterventionId());
+		document.put("isClose", entity.isClose());
+		document.put("positions", Tools.positionListToBasicDBList(entity.getPositions()));
+		return document;
+	}
+
+}
